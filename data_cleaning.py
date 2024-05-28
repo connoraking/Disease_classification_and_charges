2e1034a66517443f1702679219375e6d91f9007f
connoraking <ckingdata@gmail.com>
2024-05-28 00:06:01 -0400

    data cleaning and adding older eda R file

---
+++ data_cleaning.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pandas as pd
+import janitor 
+
+# Reading in data 
+
+path_project = "C:/Users/Conno/Documents/Career/Projects/Hospital_Charges"
+
+os.chdir(path_project)
+
+support2_path = os.path.join("data", "support2.csv")
+
+df_raw = pd.read_csv(support2_path)
+
+# Removing columns with more than 25% NA
+df_c = df_raw.loc[:, df_raw.notna().mean() > 0.75]
+df_c = df_c.dropna()
+
+
+# Some pre-processing of the variables, preparing for one-hot encoding
+cat_columns = ["num.co", "dzclass", "dzgroup"]
+df_c[cat_columns] = df_c[cat_columns].apply(lambda x: x.astype('category'))
+df_c["num.co"] = df_c["num.co"].astype("category") # converting to categorical variable (equivalent to factor in R)
+df_c = df_c.drop(columns = ["totcst"]) # these columns use "charges" in their calculations
+
+# One-hot encoding the categorical predictors, removes first to avoid multicollinearity
+df_encoded = pd.get_dummies(df_c, drop_first = True)
+
+# Cleaning column names using pyjanitor
+df_encoded = df_encoded.clean_names()
+
+# Removing columns with near 0 variance as this will cause significant problems some certain models (neural networks specifically)
+variances = df_encoded.var()
+var_threshold = 0.01
+cols_drop = variances[variances < var_threshold].index # removed num_co_7 thru num_co_9, race_other and sfdm2_coma_or_intub
+cols_drop = cols_drop.tolist()
+
+df_processed = df_encoded.drop(columns = cols_drop)
+
+# Uploading as CSV
+df_processed.to_csv("./df_processed.csv")
